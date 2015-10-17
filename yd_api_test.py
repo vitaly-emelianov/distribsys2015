6c0deaf469ccecaef5335f99d43435317765a3c4
EmelianovVitaly <vitalik.emelianov@gmail.com>
2015-10-17 20:39:06 +0300

    all functional implemented

--- yd_api_test.py
+++ yd_api_test.py
@@ -8,20 +8,34 @@ URL = "https://cloud-api.yandex.net/v1/disk/resources"
 
 
 def ls(path):
-    params = {"path" : path}
-    headers = {"Authorization" : "OAuth " + TOKEN}
+    URL = "https://cloud-api.yandex.net/v1/disk/resources"
+    params = {"path": path}
+    headers = {"Authorization": "OAuth " + TOKEN}
     r = requests.get(URL, params=params, headers=headers)
     for el in r.json()['_embedded']['items']:
         print("{0}  {1}".format(el['name'], el['modified']))
 
-def rm():
-    pass
 
-def mv():
-    pass
+def rm(path):
+    URL = "https://cloud-api.yandex.net/v1/disk/resources"
+    params = {"path": path}
+    headers = {"Authorization": "OAuth " + TOKEN}
+    r = requests.delete(URL, params=params, headers=headers)
 
-def cp():
-    pass
+
+def cp(path, destination):
+    URL = "https://cloud-api.yandex.net/v1/disk/resources/copy"
+    params = {"from": path, "path": destination}
+    headers = {"Authorization": "OAuth " + TOKEN}
+    r = requests.post(URL, params=params, headers=headers)
+    print(r.json())
+
+
+def mv(path, destination):
+    URL = "https://cloud-api.yandex.net/v1/disk/resources/move"
+    params = {"from": path, "path": destination}
+    headers = {"Authorization": "OAuth " + TOKEN}
+    r = requests.post(URL, params=params, headers=headers)
 
 
 if __name__ == '__main__':
@@ -29,16 +43,15 @@ if __name__ == '__main__':
     choices = {'ls': ls, 'rm': rm, 'mv': mv, 'cp': cp}
     parser = argparse.ArgumentParser()
     parser.add_argument('operation', choices=choices, help='which operation to do')
-    parser.add_argument('dir')
+    parser.add_argument('path')
+    parser.add_argument('destination')
     args = parser.parse_args()
 
     if args.operation == 'ls':
-        ls(args.dir)
+        ls(args.path)
     elif args.operation == 'rm':
-        pass
+        rm(args.path)
     elif args.operation == 'mv':
-        pass
+        mv(args.path, args.destination)
     elif args.operation == 'cp':
-        pass
-    else:
-        print("Bad argument.")
+        cp(args.path, args.destination)
