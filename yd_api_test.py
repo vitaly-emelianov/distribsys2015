0a82b8de9b76d902725d09180a87e1e2e9c460a8
EmelianovVitaly <vitalik.emelianov@gmail.com>
2015-10-17 16:21:34 +0300

    updated

--- yd_api_test.py
+++ yd_api_test.py
@@ -11,8 +11,8 @@ def ls(path):
     params = {"path" : path}
     headers = {"Authorization" : "OAuth " + TOKEN}
     r = requests.get(URL, params=params, headers=headers)
-    print(r.status_code, r.reason)
-    print(r.json())
+    for el in r.json()['_embedded']['items']:
+        print("{0}  {1}".format(el['name'], el['modified']))
 
 def rm():
     pass
@@ -29,10 +29,11 @@ if __name__ == '__main__':
     choices = {'ls': ls, 'rm': rm, 'mv': mv, 'cp': cp}
     parser = argparse.ArgumentParser()
     parser.add_argument('operation', choices=choices, help='which operation to do')
+    parser.add_argument('dir')
     args = parser.parse_args()
 
     if args.operation == 'ls':
-        ls("/")
+        ls(args.dir)
     elif args.operation == 'rm':
         pass
     elif args.operation == 'mv':
