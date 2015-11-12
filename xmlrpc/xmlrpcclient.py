e6430dc41f0f44f2d99f4b06a02b9a8fa80fc25a
EmelianovVitaly <vitalik.emelianov@gmail.com>
2015-11-12 16:46:36 +0300

    deleted unuseable folders

--- xmlrpc/xmlrpcclient.py
+++
@@ -1,5 +0,0 @@
-from ServerProxy import ServerProxy 
-
-proxy = ServerProxy("http://localhost:8001/")
-print("3 is even: %s" % str(proxy.div(3)))
-print("100 is even: %s" % str(proxy.div(12, 3)))
--- xmlrpc/xmlrpcserver.py
+++
@@ -1,9 +0,0 @@
-from SimpleXMLRPCServer import SimpleXMLRPCServer
-
-def div(a, b):
-    return a // b
-
-server = SimpleXMLRPCServer(("localhost", 8001))
-print("Listening on port 8001...")
-server.register_function(div, "div")
-server.serve_forever()
